60b6fadf191bdd6844c63e90a52a6a9d087922f3
Omar Aflak <aflakomar@gmail.com>
2022-01-26 08:17:23 +0000

    add eip creator badge contract

---
+++ contracts/EIPCreatorBadge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./NTT.sol";
+
+contract EIPCreatorBadge is NTT {
+    constructor() NTT("EIP Creator Badge", "EIP") {}
+
+    function giveThatManABadge(address owner) external {
+        require(_isCreator(), "You must be the contract creator");
+        _mint(owner);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://eips.ethereum.org/ntt/";
+    }
+}
